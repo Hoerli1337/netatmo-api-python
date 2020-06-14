9c87ba0015178d887697bb0d0d941e2cb790f61c
Hoerli <13108516+Hoerli1337@users.noreply.github.com>
2020-06-14 17:31:03 +0200

    Update description

--- show-stations.py
+++ show-stations.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 # encoding=utf-8
+#
+# This File show all Stations, that are connectet to your Account
+# For the query to work, a valid API key and login must be entered in the .netatmo-credentials
 
 import time
 import lnetatmo
@@ -18,3 +21,8 @@ for station in weather.stations:
 	print(weather.stations[station]["station_name"] + " -> " +station)
 	continue
 
+# Output example
+# Station owner : Your E-Mail
+# Data units : metric
+# Station-Name1 : AA:BB:CC:DD:EE:FF
+# Station-Name2 : GG:HH:II:JJ:KK:LL
